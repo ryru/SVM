5d2bab146d93c726dc9733c3b7927fc2452e4549
Pascal Knecht <pascal.knecht@hsr.ch>
2019-12-08 15:33:04 +0100

    remove unnecessary imports

--- src/main/java/ch/addere/Main.java
+++ src/main/java/ch/addere/Main.java
@@ -1,13 +1,7 @@
 package ch.addere;
 
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 
 public class Main {
 
