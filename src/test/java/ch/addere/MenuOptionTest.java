aabb9559c90540d80e36801ffde7d77410f4da8c
Pascal Knecht <pascal.knecht@hsr.ch>
2019-12-08 13:38:52 +0100

    add more MenuOption tests

--- src/test/java/ch/addere/MenuOptionTest.java
+++ src/test/java/ch/addere/MenuOptionTest.java
@@ -1,6 +1,7 @@
 package ch.addere;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -26,5 +27,35 @@ class MenuOptionTest {
         "Herkunft: Kabeljau / Nord-Ost Atlantik\n" +
         "INT " + String.format("%.2f", 8.0) + " | EXT " + String.format("%.2f", 12.0);
     assertEquals(expected, menu1.toString());
+  }
+
+  @Test
+  void testMenuGetDate() {
+    assertEquals("Fr 22.11.", menu1.getDate());
+  }
+
+  @Test
+  void testMenuGetTitle() {
+    assertEquals("Paniertes MSC Kabeljau Filet", menu1.getTitle());
+  }
+
+  @Test
+  void testMenuGetDescription() {
+    assertEquals("mit Mayonnaise, Petersilien-Kartoffeln und Lattich mit Zwiebeln", menu1.getDescription());
+  }
+
+  @Test
+  void testMenuGetProvenance() {
+    assertEquals("Herkunft: Kabeljau / Nord-Ost Atlantik", menu1.getProvenance());
+  }
+
+  @Test
+  void testMenuGetPrice() {
+    assertEquals(new Price(8.0, 12.0).toString(), menu1.getPrice());
+  }
+
+  @Test
+  void testMenuIsVegetarian() {
+    assertFalse(menu1.isVegetarian());
   }
 }
