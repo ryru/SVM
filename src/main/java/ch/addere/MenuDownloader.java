aa0970833cd4ba48775f25ed1e491f5f6e5d975b
Pascal Knecht <pascal.knecht@hsr.ch>
2019-12-08 15:32:16 +0100

    pack InputStream in try block

--- src/main/java/ch/addere/MenuDownloader.java
+++ src/main/java/ch/addere/MenuDownloader.java
@@ -10,11 +10,18 @@ import java.nio.file.StandardCopyOption;
 
 public class MenuDownloader {
 
+  private MenuDownloader() {
+  }
+
   static File downloadMenu(String url) throws IOException {
-    InputStream in = new URL(url).openStream();
-    File tempFile = File.createTempFile("mensa","html");
-    tempFile.deleteOnExit();
-    Files.copy(in, Paths.get(tempFile.toURI()), StandardCopyOption.REPLACE_EXISTING);
+    File tempFile;
+
+    try (InputStream in = new URL(url).openStream()) {
+      tempFile = File.createTempFile("mensa", "html");
+      tempFile.deleteOnExit();
+      Files.copy(in, Paths.get(tempFile.toURI()), StandardCopyOption.REPLACE_EXISTING);
+
+    }
 
     return tempFile;
   }
